b96bca391858eaf613e8a8486d093227f4d347fc
Alex Collins <alex.e.c@gmail.com>
2012-05-17 21:37:51 +0100

    @Transactional

--- src/main/java/tutorial/UsersController.java
+++ src/main/java/tutorial/UsersController.java
@@ -1,8 +1,10 @@
 package tutorial;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -22,5 +24,22 @@ public class UsersController {
         model.addAttribute("users", entityManager.createQuery("select u from User u").getResultList());
 
         return "users";
+    }
+
+    @RequestMapping(value = "/create-user", method = RequestMethod.GET)
+    public String createUser(Model model) {
+        return "create-user";
+    }
+
+    @RequestMapping(value = "/create-user", method = RequestMethod.POST)
+    @Transactional
+    public String createUser(Model model, String name) {
+
+        User user = new User();
+        user.setName(name);
+
+        entityManager.persist(user);
+
+        return "redirect:/users.html";
     }
 }

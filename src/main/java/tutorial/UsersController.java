3d2533669454891bf549f5fe8459c9cd3bd98855
Alex Collins <alex.e.c@gmail.com>
2012-05-17 10:51:19 +0100

    Entity manager.

--- src/main/java/tutorial/UsersController.java
+++ src/main/java/tutorial/UsersController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 /**
  * @author: alexec (alex.e.c@gmail.com)
  */
 @Controller
 public class UsersController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @RequestMapping("/users")
     public String users(Model model) {
+
+        model.addAttribute("users", entityManager.createQuery("select u from User u").getResultList());
+
         return "users";
     }
 }
--- src/main/webapp/WEB-INF/pages/users.jsp
+++ src/main/webapp/WEB-INF/pages/users.jsp
@@ -1,5 +1,14 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
 <body>
 <h1>Users</h1>
+<table>
+    <thead><tr><td>ID</td><td>Name</td></tr></thead>
+    <tbody>
+        <c:forEach var="user" items="${users}">
+        <tr><td>${user.id}</td><td>${user.name}</td></tr>
+        </c:forEach>
+    </tbody>
+</table>
 </body>
 </html>
